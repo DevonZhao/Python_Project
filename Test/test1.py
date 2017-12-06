76d58a4ef7f7798951574f2ae55af83c2ed4701b
DevonZhao <donyorcl@gmail.com>
2017-12-06 17:17:36 +0800

    Initial commit

---
+++ Test/test1.py
@@ -0,0 +1,9 @@
+# realize a function
+def test(a,b,func):
+    result = func(a,b)
+    print(result)
+
+func_new = input("please input a funciton:")
+print type(func_new)
+# func_new = eval(func_new)
+test(2,3,func_new)
