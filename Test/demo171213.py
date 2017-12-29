2ee550f297c8ee7694aae7a2acc6208019d161c2
canger <canger@7lk.com>
2017-12-29 15:51:33 +0800

    commit on 20171229

--- Test/demo171213.py
+++ Test/demo171213.py
@@ -8,34 +8,71 @@
 # Usage         :python test4.py
 # Notes         :
 # python_version     :2.7.14
-#==============================================================================
+# 进一步完善：封装成一个类，然后对输出邮件的内容进行格式化。
+# ==============================================================================
 
+import os, sys
+import smtplib
 
-import os,sys
+from subprocess import Popen, PIPE
+from email.header import Header
+from email.mime.text import MIMEText
 import MySQLdb
 
-#
-# print os.system('pwd')
-#
-# db = MySQLdb.connect("192.168.56.101","devon","123123", "mysql")
-# cursor = db.cursor()
-# cursor.execute("show all slaves status;")
-# d = cursor.fetchall()
-# print type(data),data
-# db.close()
-
-d = (('dn1', 'Slave has read all relay log; waiting for the slave I/O thread to update it', 'Waiting for master to send event', '10.10.5.6', 'repl', 8088L, 60L, 'binlog.000096', 69607055L, 'relay-bin-dn1.000083', 69607344L, 'binlog.000096', 'Yes', 'Yes', '', 'new_medic_assist.*,medic_assist.*,third_party_medic_assist.*,7lk_iplatform.nms_operation_log,rundeck.*', '', '', '', 'information_schema.%,performance_schema.%,mysql.%,test.%,salepromotion.%,sync_info.%,tungsten_gz_odc.%,tungsten_crm.%,fss.%,partner.%,pdc.%,permission.%,vorder.%,tungsten_pdc.%,base_commodity.%,baymax_doctor_weixin.%,bi_report.%,medic_assist.%,new_medic_assist.%,third_party_medic_assist.%,tungsten_migration_service.%,tungsten_ods33.%', 0L, '', 0L, 69607055L, 69607688L, 'None', '', 0L, 'No', '', '', '', '', '', 0L, 'No', 0L, '', 0L, '', '', 10100506L, '', '', 'No', '', '', '', 'conservative', 0L, 1073741824L, 3426748L, 0L, 1800.0, ''), ('dn2', 'Slave has read all relay log; waiting for the slave I/O thread to update it', 'Waiting for master to send event', '10.10.56.18', 'repl_to_slave', 3306L, 60L, 'binlog.000010', 501729699L, 'relay-bin-dn2.000029', 8503866L, 'binlog.000010', 'Yes', 'Yes', '', 'new_medic_assist.*,medic_assist.*,third_party_medic_assist.*,7lk_iplatform.nms_operation_log,rundeck.*', '', '', '', 'information_schema.%,performance_schema.%,mysql.%,test.%,salepromotion.%,sync_info.%,tungsten_gz_odc.%,tungsten_crm.%,fss.%,partner.%,pdc.%,permission.%,vorder.%,tungsten_pdc.%,base_commodity.%,baymax_doctor_weixin.%,bi_report.%,medic_assist.%,new_medic_assist.%,third_party_medic_assist.%,tungsten_migration_service.%,tungsten_ods33.%', 0L, '', 0L, 501729699L, 8504162L, 'None', '', 0L, 'No', '', '', '', '', '', 0L, 'No', 0L, '', 0L, '', '', 180L, '', '', 'No', '', '', '', 'conservative', 0L, 1073741824L, 73110L, 1L, 1800.0, ''), ('dn3', 'Slave has read all relay log; waiting for the slave I/O thread to update it', 'Waiting for master to send event', '10.10.5.4', 'repl', 3306L, 60L, 'binlog.000077', 854254501L, 'relay-bin-dn3.000061', 145961567L, 'binlog.000077', 'Yes', 'Yes', '', 'new_medic_assist.*,medic_assist.*,third_party_medic_assist.*,7lk_iplatform.nms_operation_log,rundeck.*', '', '', '', 'information_schema.%,performance_schema.%,mysql.%,test.%,salepromotion.%,sync_info.%,tungsten_gz_odc.%,tungsten_crm.%,fss.%,partner.%,pdc.%,permission.%,vorder.%,tungsten_pdc.%,base_commodity.%,baymax_doctor_weixin.%,bi_report.%,medic_assist.%,new_medic_assist.%,third_party_medic_assist.%,tungsten_migration_service.%,tungsten_ods33.%', 0L, '', 0L, 854254501L, 145961863L, 'None', '', 0L, 'No', '', '', '', '', '', 0L, 'No', 0L, '', 0L, '', '', 4L, '', '', 'No', '', '', '', 'conservative', 0L, 1073741824L, 316915L, 0L, 1800.0, ''), ('dn4', 'Slave has read all relay log; waiting for the slave I/O thread to update it', 'Waiting for master to send event', '10.10.1.254', 'repl', 3306L, 60L, 'binlog.003864', 708397964L, 'relay-bin-dn4.002511', 708398253L, 'binlog.003864', 'Yes', 'Yes', '', 'new_medic_assist.*,medic_assist.*,third_party_medic_assist.*,7lk_iplatform.nms_operation_log,rundeck.*', '', '', '', 'information_schema.%,performance_schema.%,mysql.%,test.%,salepromotion.%,sync_info.%,tungsten_gz_odc.%,tungsten_crm.%,fss.%,partner.%,pdc.%,permission.%,vorder.%,tungsten_pdc.%,base_commodity.%,baymax_doctor_weixin.%,bi_report.%,medic_assist.%,new_medic_assist.%,third_party_medic_assist.%,tungsten_migration_service.%,tungsten_ods33.%', 0L, '', 0L, 708397964L, 708398597L, 'None', '', 0L, 'No', '', '', '', '', '', 0L, 'No', 0L, '', 0L, '', '', 254L, '', '', 'No', '', '', '', 'conservative', 0L, 1073741824L, 22819012L, 0L, 1800.0, ''))
-for i in range(len(d)):
-    print "Connection_name: "+d[i][0]
-    print "master server ip: "+d[i][3]
-    print "Slave_IO_Running: " + d[i][12]
-    print "Slave_SQL_Running: " + d[i][13]
-    print "Seconds_Behind_Master: " + str(d[i][34])
-    if d[i][12] is "Yes" and d[i][13] is "Yes":
-        print 1
-    else:
-        print 0
+reload(sys)
+sys.setdefaultencoding('utf-8')
+# 如果是多个邮件则使用逗号分开
+to_list = ['@.com']
+server_host = '..com'
+username = '@.com'
+password = ''
 
 
+def get_data():
+    db = MySQLdb.connect("192.168..", "", "")
+    cursor = db.cursor()
+    # cursor.execute("show all slaves status;")
+    cursor.execute("show all slaves status;")
+    d = cursor.fetchall()
+    db.close()
+    return d
+
+
+def send_mail(to_list, sub, content):
+    me = "manager" + "<" + username + ">"
+    msg = MIMEText(content, _subtype='html')
+    msg['Subject'] = Header(sub, 'utf-8').encode()
+    msg['From'] = me
+    msg['To'] = ';'.join(to_list)
+    try:
+        server = smtplib.SMTP()
+        server.connect(server_host)
+        server.login(username, password)
+        server.sendmail(me, to_list, msg.as_string())
+        server.close()
+    except Exception as e:
+        print str(e)
+
+
+def parse_data():
+    d = get_data()
+    for i in range(len(d)):
+        # print "Connection_name: "+d[i][0]
+        # print "master server ip: "+d[i][3]
+        # print "Slave_IO_Running: " + d[i][12]
+        # print "Slave_SQL_Running: " + d[i][13]
+        # print "Seconds_Behind_Master: " + str(d[i][34])
+        mail_content = 'The following nodes have been down:    \n node name is:' + d[i][0] + '     \n master ip is:' + \
+                       d[i][3] + '     \n Slave_IO_Running status is:' + d[i][
+                           12] + '     \n Slave_SQL_Running status is:' + d[i][13]
+        if d[i][12] == "Yes" and d[i][13] == "Yes":
+            print 1
+        else:
+            print 0
+            send_mail(to_list, 'BigData Mysql slave status:', mail_content)
+
+
+if __name__ == '__main__':
+    parse_data()
 
 
